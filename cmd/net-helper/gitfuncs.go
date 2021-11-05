bc32c85ae17a04219712e9938e88860462ace9f3
skyneticist <hunterhartline87@gmail.com>
2021-11-05 15:52:09 -0500

    remove unused gitGetBranch call in ACPR

--- cmd/net-helper/gitfuncs.go
+++ cmd/net-helper/gitfuncs.go
@@ -114,10 +114,6 @@ func AddCommitPush(c *cli.Context) error {
 // AddCommitPushRemote - Add, Commit, Push local changes
 // on fresh branch (sets upstream)
 func AddCommitPushRemote(c *cli.Context) error {
-	// gitBranch, err := getGitBranch()
-	// if err != nil {
-	// 	return err
-	// }
 	commitMsg := os.Args[2]
 	cmds := GitCmdList{
 		GitCmd{
